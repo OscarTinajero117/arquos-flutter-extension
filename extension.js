f6c488e98491724ada899fa01a2ad4526bcd4688
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-01-02 16:00:43 -0600

    change dialog to Spanish

--- extension.js
+++ extension.js
@@ -21,7 +21,7 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from arquos-flutter-extension!');
+		vscode.window.showInformationMessage('Hola Mundo desde arquos-flutter-extension!');
 	});
 
 	context.subscriptions.push(disposable);
